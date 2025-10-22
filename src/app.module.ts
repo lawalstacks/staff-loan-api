47efae1023943a289c36148417fb70783ee23fea
kazeem <quazeemastic@gmail.com>
2025-10-22 16:00:31 +0100

    chore: add global middlewares
    - Added helmet, morgan, and cors to main.ts.
    - Configured @nestjs/throttler for global rate limiting.
    - Applied global HttpExceptionFilter.
    - Strengthened global ValidationPipe.
    - The backend is now feature-complete.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,28 @@ import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { LoansModule } from './loans/loans.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 60 seconds
+        limit: 10,  // 10 requests
+      },
+    ]),
     AuthModule, LoansModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(helmet());
+  app.use(morgan('dev'));
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Strips non-DTO properties
+    forbidNonWhitelisted: true, // Throws error on non-DTO properties
+  }));
+
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
