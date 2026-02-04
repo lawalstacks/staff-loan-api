75635392f7aed6b99114755a2345f3d55eae47f7
Kazeem lawal <49644714+lawalstacks@users.noreply.github.com>
2026-02-04 15:56:41 +0100

    Update CORS origin for API access

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://staff-loan.edgeone.app',
+      'https://staff-loan-api.miiglu.app',
       'http://localhost:5173'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
