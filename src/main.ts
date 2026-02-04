4d350fe7a9fecd4bb230ae94cc8ef02571a57459
Kazeem lawal <49644714+lawalstacks@users.noreply.github.com>
2026-02-04 15:59:47 +0100

    Update CORS origin for staff loan UI

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://staff-loan-api.miiglu.app',
+      'https://staff-loan-ui.miiglu.app/login',
       'http://localhost:5173'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
