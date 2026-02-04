1d6364b7bad8278683028633609b97dc36923107
Kazeem lawal <49644714+lawalstacks@users.noreply.github.com>
2026-02-04 16:00:04 +0100

    Update CORS origin for production URL

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://staff-loan-ui.miiglu.app/login',
+      'https://staff-loan-ui.miiglu.app',
       'http://localhost:5173'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
